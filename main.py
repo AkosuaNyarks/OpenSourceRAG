f1e7fb5abece4cb3f7be9dcf060b41939bdb47c7
Akosua Aidoo <aknyarkomah@gmail.com>
2026-07-01 12:48:24 -0400

    Connected pipeline in main

---
+++ main.py
@@ -0,0 +1,37 @@
+from ingestion.load_docs import load_documents
+from ingestion.transformations import split_documents
+from indexing.build_index import build_index
+from retrieval.retriever import  get_retriever
+from llm.llm_factory import get_llm
+from retrieval.query_engine import get_query_engine
+def main():
+    # 1. Load the documents
+    load_docs=load_documents()
+
+    #2.Split the documents into nodes
+    nodes=split_documents(load_docs)
+
+    #3. Build an index for the nodes and store in ChromaDB
+    index=build_index(nodes)
+
+    #4. Create a retriever to retrieve the data  that...
+    retriever=get_retriever(index)
+
+    #5. Setup the LLM model
+    llm=get_llm()
+
+    #6.send the retriver and the llm to the query engine
+    query_engine=get_query_engine(retriever,llm)
+
+    #Ask a question
+    question = "When was Nvidia founded, and by whom?"
+
+
+    response=query_engine.query(question)
+    print (response)
+
+
+
+
+if __name__ =="__main__":
+    main()
