911eea2f587660b7f1b06ea4d03aaefa0ef30ba8
Akosua Aidoo <aknyarkomah@gmail.com>
2026-07-21 10:35:21 -0400

    Refactor Code

--- llm/llm_factory.py
+++ llm/llm_factory.py
@@ -1,17 +1,21 @@
+import os
+from typing import Optional
 from llama_index.llms.openai import OpenAI
 from llama_index.core.llms import LLM
 
-def get_llm(model:str="gpt-3.5-turbo") -> LLM:
+def get_llm(model:Optional[str]=None,api_key:Optional[str]= None) -> LLM:
     """
     Get access to LLM
 
         Args:
             model=Name of the OpenAI model to use
+            api_key = OPEN AI API Key
 
         Return:
             A large language model instance
     """
-
-    return OpenAI(model=model)
-
-llm=get_llm()
+    if model is None:
+        model=os.environ.get("OPENAI_MODEL","gpt-4o-mini")
+    if api_key is None:
+        api_key=os.environ.get("OPENAI_API_KEY")
+    return OpenAI(model=model,api_key=api_key)
