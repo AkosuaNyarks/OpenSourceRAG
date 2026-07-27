33b0e34a93f0f2d416bea1d521e6018eb94a7dfa
Akosua Aidoo <aknyarkomah@gmail.com>
2026-07-26 20:42:01 -0400

    Increase similarity_top_k from 2 to 8 in retriever,py to fix under-retrieval

--- retrieval/retriever.py
+++ retrieval/retriever.py
@@ -3,6 +3,6 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.base.base_retriever import BaseRetriever
 
 
-def get_retriever(index:VectorStoreIndex)->BaseRetriever:
-    retriever = index.as_retriever()
+def get_retriever(index:VectorStoreIndex,top_k:int=8)->BaseRetriever:
+    retriever = index.as_retriever(similarity_top_k=top_k)
     return retriever
